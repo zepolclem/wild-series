3bd6f322a9087d992d4df71a6165f1eb5cf219eb
zepolclem <lopez.clmnt@gmail.com>
2020-12-18 14:32:28 +0100

    Revert "quest 05"
    This reverts commit 32f3be89e41a372d01eb29db2c6f506dbf9155bf.

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,24 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/programs", name="program_")
- */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/programs/", name="program_index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', ['website' => 'Wild Séries']);
-    }
-
-    /**
-     * @Route("/{id}", name="show", requirements={"id"="\d+"}, methods={"GET"},)
-     */
-    public function show($id): Response
-    {
-        return $this->render('program/show.html.twig', ['id' => $id]);
     }
 }
